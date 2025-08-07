a2d32470293331ca0478bae39729d17848b97f36
iamnobita206243 <nobitamusic001@gmail.com>
2025-08-08 02:53:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
 import time
-import datetime
-print(f"Current time: {datetime.datetime.now()}")
-time.sleep(5)
-
-import pyrogram
 import logging
 import os
+
 from config import Config
-from pyrogram import Client 
+from pyrogram import Client
+
+# Wait to sync time with Telegram servers
+time.sleep(5)
 
 # Logging setup
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 if __name__ == "__main__":
     plugins = dict(root="plugins")
+
     app = Client(
-        "ShowJson",
+        "Showyson",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
@@ -26,4 +28,5 @@ if __name__ == "__main__":
         workers=100,
         sleep_threshold=60
     )
+
     app.run()
