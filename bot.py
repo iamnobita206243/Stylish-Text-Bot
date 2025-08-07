de1d792c6015d0d77446aeab604d594cefe3b942
iamnobita206243 <nobitamusic001@gmail.com>
2025-08-08 02:27:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
 import pyrogram
-
 import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 import os
 
 from config import Config
 from pyrogram import Client 
+
+# Logging setup
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     plugins = dict(
         root="plugins"
     )
@@ -22,6 +21,7 @@ if __name__ == "__main__" :
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
-        workers=100
+        workers=100,
+        **sleep_threshold=60**   # 🛠 Add this line to fix time sync issue
     )
     app.run()
